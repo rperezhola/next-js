7da9c48d0939777960c9d342bc7b0a8d7c5ee69a
root <rperez@systools.hola.com>
2022-03-15 16:12:10 +0100

    change footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 import { FooterContainer } from "./styles";
 
 export default function Footer() {
-    return <FooterContainer>GCP Footer</FooterContainer>;
+    return <FooterContainer>Cloud Run Footer</FooterContainer>;
 }
